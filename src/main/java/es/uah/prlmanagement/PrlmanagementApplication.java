e0c5fe08772223b998e2dddc085d2e64fdee30b2
Alberto Murillo <albmurillop@gmail.com>
2021-07-03 11:09:36 +0200

    Added the configuration of Spring Initializr

---
+++ src/main/java/es/uah/prlmanagement/PrlmanagementApplication.java
@@ -0,0 +1,13 @@
+package es.uah.prlmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrlmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrlmanagementApplication.class, args);
+	}
+
+}
